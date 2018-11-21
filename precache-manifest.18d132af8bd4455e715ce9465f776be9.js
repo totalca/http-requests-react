7c04844f11c9c76e90cd4c4378d78d372027abe3
Nicolae Totalca <totalca@yahoo.com>
2018-11-21 00:34:10 +0000

    Updates

---
+++ precache-manifest.18d132af8bd4455e715ce9465f776be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bac83854b47ea58c927",
+    "url": "/react-fun/static/js/runtime~main.8bac8385.js"
+  },
+  {
+    "revision": "a22ef2a9b28634b0ba30",
+    "url": "/react-fun/static/js/main.a22ef2a9.chunk.js"
+  },
+  {
+    "revision": "7ac19a4e69f63e01a861",
+    "url": "/react-fun/static/js/1.7ac19a4e.chunk.js"
+  },
+  {
+    "revision": "a22ef2a9b28634b0ba30",
+    "url": "/react-fun/static/css/main.2a51c4cc.chunk.css"
+  },
+  {
+    "revision": "6aaf479af9ec05c16e108c99f22eb092",
+    "url": "/react-fun/index.html"
+  }
+];
